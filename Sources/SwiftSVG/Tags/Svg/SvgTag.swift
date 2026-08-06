ab39bca6fb8a954720122f5afae7bc95abc2e4e6
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-08-06 16:04:57 +0400

    Add SvgTag

---
+++ Sources/SwiftSVG/Tags/Svg/SvgTag.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Kurlovich Vitali on 8/3/26.
+//
+
+public struct SvgTag: SVGMutableElement, Sendable {
+    public static var name: String {
+        "svg"
+    }
+
+    public var attributes: [String: String] = [:]
+    public var childs: [any SVGElement] = []
+
+    init(attributes: [String: String] = [:], childs: [any SVGElement] = []) {
+        self.attributes = attributes
+        self.childs = childs
+    }
+}

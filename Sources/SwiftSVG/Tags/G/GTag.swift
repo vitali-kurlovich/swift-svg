ddf73902fe71e5197e5fefb7d8a0b5e450eeca21
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-08-06 16:05:58 +0400

    Add GTag

---
+++ Sources/SwiftSVG/Tags/G/GTag.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Kurlovich Vitali on 8/3/26.
+//
+
+public struct GTag: SVGMutableElement {
+    public static var name: String {
+        "g"
+    }
+
+    public var attributes: [String: String] = [:]
+    public var childs: [any SVGElement] = []
+
+    init(attributes: [String: String] = [:], childs: [any SVGElement] = []) {
+        self.attributes = attributes
+        self.childs = childs
+    }
+}

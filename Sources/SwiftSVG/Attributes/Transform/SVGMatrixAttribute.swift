464a6da90a38bc69319e8eb2b2a5393bc6676f82
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-08-07 11:31:06 +0400

    Add SVGMatrixAttribute

---
+++ Sources/SwiftSVG/Attributes/Transform/SVGMatrixAttribute.swift
@@ -0,0 +1,53 @@
+//
+//  Created by Kurlovich Vitali on 8/7/26.
+//
+
+public struct SVGMatrixAttribute: Sendable, Hashable {
+    public let a: Double
+    public let b: Double
+    public let c: Double
+    public let d: Double
+
+    public let tx: Double
+    public let ty: Double
+
+    @inlinable
+    public init(
+        a: Double,
+        b: Double,
+        c: Double,
+        d: Double,
+        tx: Double,
+        ty: Double,
+    ) {
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.tx = tx
+        self.ty = ty
+    }
+}
+
+extension SVGMatrixAttribute {
+    init?(_ string: some StringProtocol) {
+        if string.hasPrefix("matrix(") {
+            let mt = string.dropFirst("matrix(".count).dropLast()
+
+            var iterator = SplitIterator(string: string, separator: ",")
+
+            let a = Double(iterator.next() ?? "1") ?? 1
+            let b = Double(iterator.next() ?? "0") ?? 0
+            let c = Double(iterator.next() ?? "0") ?? 0
+            let d = Double(iterator.next() ?? "1") ?? 1
+
+            let tx = Double(iterator.next() ?? "0") ?? 0
+            let ty = Double(iterator.next() ?? "0") ?? 0
+
+            self.init(a: a, b: b, c: c, d: d, tx: tx, ty: ty)
+
+        } else {
+            return nil
+        }
+    }
+}

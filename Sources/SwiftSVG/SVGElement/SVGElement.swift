58f9ad552d9353c11435858a8bde9c5270436093
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-08-11 16:36:40 +0400

    Support Equatable for SVGElement

--- Sources/SwiftSVG/SVGElement/SVGElement.swift
+++ Sources/SwiftSVG/SVGElement/SVGElement.swift
@@ -2,7 +2,7 @@
 //  Created by Kurlovich Vitali on 8/3/26.
 //
 
-public protocol SVGElement: Sendable {
+public protocol SVGElement: Equatable, Sendable {
     static var name: String { get }
 
     var attributes: [String: String] { get }
@@ -15,7 +15,37 @@ public extension SVGElement {
     }
 }
 
+public extension SVGElement {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        isEquals(lhs: lhs, rhs: rhs)
+    }
+}
+
 public protocol SVGMutableElement: SVGElement {
     var attributes: [String: String] { get set }
     var childs: [any SVGElement] { get set }
+}
+
+public extension SVGElement {
+    static func isEquals(lhs: borrowing any SVGElement, rhs: borrowing any SVGElement) -> Bool {
+        guard lhs.name == rhs.name, lhs.attributes == rhs.attributes else {
+            return false
+        }
+
+        return isEquals(lhs: lhs.childs, rhs: rhs.childs)
+    }
+
+    static func isEquals(lhs: borrowing [any SVGElement], rhs: borrowing [any SVGElement]) -> Bool {
+        guard lhs.count == rhs.count else {
+            return false
+        }
+
+        for (left, right) in zip(lhs, rhs) {
+            guard isEquals(lhs: left, rhs: right) else {
+                return false
+            }
+        }
+
+        return true
+    }
 }

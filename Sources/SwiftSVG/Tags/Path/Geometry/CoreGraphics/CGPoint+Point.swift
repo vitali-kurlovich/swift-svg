d98620fb90f2d3aa707e16f5f3bca3a9006d1a00
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-08-12 17:47:40 +0400

    Initialize Point with  CGPoint

--- Sources/SwiftSVG/Tags/Path/Geometry/CoreGraphics/CGPoint+Point.swift
+++ Sources/SwiftSVG/Tags/Path/Geometry/CoreGraphics/CGPoint+Point.swift
@@ -5,6 +5,13 @@
 import struct CoreGraphics.CGFloat
 import struct CoreGraphics.CGPoint
 
+public extension Point {
+    @inlinable
+    init(_ p: CGPoint) {
+        self.init(x: p.x, y: p.y)
+    }
+}
+
 public extension CGPoint {
     @inlinable
     init(_ p: Point) {

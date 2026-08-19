ca65ef77b58cf8c56b7180442c30cce4aa1eeab6
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-08-19 14:49:06 +0400

    Generate DocC action

--- Package.swift
+++ Package.swift
@@ -17,6 +17,9 @@ let package = Package(
             targets: ["SwiftSVG"],
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
